adca27138a0ded526257020394634e1f0f221d78
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-01-29 15:09:27 +0100

    Update Swagger
    Adding sample documentation about api in swagger

--- SwaggerAPI/Program.cs
+++ SwaggerAPI/Program.cs
@@ -1,9 +1,32 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        //put from appsettings/hard code
+        Version = "v1",
+        Title = "User API (title for swagger)",
+        Description = "This is description about API",
+        TermsOfService = new Uri("https://www.termsofservicesite.com"),
+        Contact = new OpenApiContact
+        {
+            Name= "ContactName",
+            Url = new Uri("https://www.contactsite.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "LicenseName",
+            Url = new Uri("https://www.licenseurl.com")
+        }
+    });
+
+});
 
 var app = builder.Build();
 
